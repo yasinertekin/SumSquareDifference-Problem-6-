b6207d1cfbcd1a9f1dc0dcf881d606d54ae30be6
Yasin Ertekin <111593895+yasinertekin@users.noreply.github.com>
2023-02-08 12:25:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0;
+            double Karetoplam = 0;
+            int ToplamKare = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+
+                sayi++;
+                
+                //Console.WriteLine(Math.Pow(sayi,2));
+                Karetoplam = Karetoplam + Math.Pow(sayi, 2);
+                ToplamKare = ToplamKare + sayi;
+            }
+            //Console.WriteLine(Math.Pow(ToplamKare,2));
+            //Console.WriteLine(Karetoplam);
+
+            double sonuc = Math.Pow(ToplamKare,2) - Karetoplam;
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
